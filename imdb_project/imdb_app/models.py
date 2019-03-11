a38ec27c1e37cc20c94a8be61b7a0968a6e03de0
ndthinh8796 <45127395+ndthinh8796@users.noreply.github.com>
2019-03-11 23:45:56 +0700

    Update models.py

--- imdb_project/imdb_app/models.py
+++ imdb_project/imdb_app/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import AbstractUser
 
 
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='photos/', blank=True)
+
+    
 class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
