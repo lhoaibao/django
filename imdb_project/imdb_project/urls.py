118b5619f42d1632ff71f3b1d1547b6d2de5e354
ndthinh8796 <45127395+ndthinh8796@users.noreply.github.com>
2019-03-11 23:43:11 +0700

    Update urls.py

--- imdb_project/imdb_project/urls.py
+++ imdb_project/imdb_project/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +25,6 @@ urlpatterns = [
     path('accounts/', include('imdb_app.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
